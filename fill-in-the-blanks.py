e7f1ddd52491e83db90a647f04d7087b5f6ed529
lassiter <pikapp336@me.com>
2018-05-17 19:20:21 -0400

    Final draft for resubmission

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -78,7 +78,7 @@ quiz = {
 def start_quiz():
   # Begins quiz by changing settings in the quiz dict. Calling difficulty_prompt to have user select difficulty
   # level which changes quiz['difficulty'] to selected difficulty level
-  # then calls the fill_blanks function to continue with quiz. 
+  # then calls the fill_blanks function to initiate the rest of the quiz. 
   quiz['running'] = True
   quiz['difficulty'] = difficulty_prompt()
   fill_blanks()
@@ -101,90 +101,109 @@ def difficulty_prompt():
 
 
 def fill_blanks():
-  # Prints appropriate paragraph, resets error count, determines if GAME OVER requirements have been met
-  paragraph = PARAGRAPHS[quiz['difficulty']]
+  # Prints game rules and starting paragraph. 
+  # Then calls question_loop defining answers_list for question_loop's input
+  quiz["paragraph"] = PARAGRAPHS[quiz['difficulty']]
+  paragraph = quiz["paragraph"]
   print '\nYou have 5 attempts per blank'
+  print "\n The paragraph reads:\n" , paragraph
   answers_list = sorted(list(ANSWERS[quiz['difficulty']].items()))
+  question_loop(answers_list)
+
+def question_loop(answers_list):
   for question, correct_answer in answers_list: 
     # Defines question (a placeholder with the format "___%d___"), 
-    # and its associated correct_answer from the "ANSWERS" dictionary to be used as the prompt for the game. 
+    #   and its associated correct_answer from the "ANSWERS" dictionary to be used as the prompt for the game. 
     # Output: prints "GAME OVER" if quiz['running'] is FALSE and ends program
-    #   If quiz['running'] remains TRUE then it returns blank_prompt function with question, correct_answer, and paragaph 
+    #   If quiz['running'] remains TRUE then it returns game_running function with question, correct_answer, and paragaph 
     #   variables
-    err_count_restart = 0
-    quiz['errorCount'] = err_count_restart
     if quiz['running'] == False:
       print '\nGAME OVER!'
       break
     else:
-      paragraph = blank_prompt(question, correct_answer, paragraph)
+      game_running(question,correct_answer)
+def game_running(question, correct_answer):
+      # Asks for user input and runs checkUserAnswer if its correct.
+      # If checkUserAnswer returns True then functions onCorrectUserAnswer and checkEndQuestions run
+      #   and loop is broken.
+      # If checkUserAnswer is False onIncorrectUserAnswer and too_many_attempts runs
+      # Inputs question, correct_answer
+      while quiz['running'] == True:
+        user_answer = askQuestion(question)
+        is_correct = checkUserAnswer(correct_answer, user_answer)
+        if (is_correct == True):
+          onCorrectUserAnswer(question, correct_answer)
+          checkEndQuestions()
+          break
 
-# def incorrect_answer(question, correct_answer, paragraph):
-#   # When blank_prompt if statment determines user input is not equal to associate correct_answer,
-#   # this function is called to output incorrect correct_answer prompt
-#   # Inputs: question, correct_answer, and paragraph are inputs derived from the fill_blanks function. 
-#   #   The question and correct_answer variables are derived from the "ANSWERS" dictionary where as the 
-#   #   paragraph variable is derived from the "PARAGRAPHS" dictionary and the selection is determined
-#   #   with the difficulty_prompt function
-#   # Outputs: Function is restarted if user_answer does not equal selected correct_answer as long as the counter
-#   #   is not over the 5 count limit.
+        else:
+          onIncorrectUserAnswer()
+          too_many_attempts()
+          
 
-#   max_tries = 5
-#   errorc_plus_one = 1 #error count + 1 when incorrect
-#   quiz['errorCount'] = quiz['errorCount'] + errorc_plus_one
-#   error_count_num = '\nIncorrect, you have ' + str(abs(quiz['errorCount'] - max_tries)) + ' attempts left!'
-#   print error_count_num
-#   if quiz['errorCount'] == max_tries: # If you exceed max tries then quiz ends
-#     quiz['running'] = False
-#   else:
-#     return blank_prompt(question, correct_answer, paragraph)
+def askQuestion(question):
+  # Prompts user to fill in the blank
+  # Inputs: question
+  # outputs user's answer to question prompt 
+  answer = raw_input( '\nFill in for blank ' + question + ':\n' + '--> ')
+  return answer
 
-# def correct_answer(correct_count, list_length, paragraph):
-#   if quiz['correctCount'] == answers_list_length:
-#    return paragraph, '\n\nCongratulations, you filled in all blanks correctly!'
-#   else:
-#     return paragraph
-
-def blank_prompt(question, correct_answer, paragraph):
-  # Prompts user for input based on the question parameter. And determines if correct_answer 
-  # is True and updates error count accordingly
-  # Inputs: question, correct_answer, and paragraph are inputs derived from the fill_blanks function. 
-  #   The question and correct_answer variables are derived from the "ANSWERS" dictionary where as the 
-  #   paragraph variable is derived from the "PARAGRAPHS" dictionary and the selection is determined
-  #   with the difficulty_prompt function 
-  # Outputs: 
-  #   If the user_answer matches the selected correct_answer then the paragraph is updated with correct correct_answer in 
-  #   place of the question placeholder and returns the updated paragraph
-  print 'The paragraph reads:'
-  print  paragraph
-
-  user_answer = raw_input( 'Fill in for blank ' + question + ':\n' + '--> ')
-  max_tries = 5
-  errorc_plus_one = 1 #error count + 1 when incorrect
-  correct_plus_one = 1 #error count + 1 when incorrect
-  answers_list_length = len(list(ANSWERS[quiz['difficulty']].items()))
-
-
-  if correct_answer.lower() != user_answer.lower():  # If user_answer does NOT equal correctanswer update errorCount + errorc_plus_one 
-    quiz['errorCount'] = quiz['errorCount'] + errorc_plus_one
-    attempts_remaining = str(abs(quiz['errorCount'] - max_tries))
-    error_count_num = '\nIncorrect, you have ' + attempts_remaining + ' attempts left!'
-    print error_count_num
-    if quiz['errorCount'] == max_tries: # If you exceed max tries then quiz ends
-      quiz['running'] = False
-      return fill_blanks()
-    else:
-      return blank_prompt(question, correct_answer, paragraph)
-
+def checkUserAnswer(correct_answer, user_answer):
+  # Checks if user_answer matches the correct_answer
+  # Inputs: correct_answer, user_answer
+  # Output: True or False
+  if (correct_answer == user_answer):
+    return True
   else:
-    quiz['correctCount'] += correct_plus_one
-    paragraph = paragraph.replace(question, correct_answer)   # Replaces question prompt in paragraph with correct_answer
-    print '\nCorrect!'
-    if quiz['correctCount'] == answers_list_length:
-      print paragraph, '\n\nCongratulations, you filled in all blanks correctly!'
-    else:
-      return paragraph
-    
+    return False
+
+def checkEndQuestions():
+  # Checks if there are any questions left in the game. 
+  # If there are none then game is won and paragraph is printed with congratulations prompt
+  # Else returns False
+  paragraph = quiz['paragraph']
+  answers_list_length = len(list(ANSWERS[quiz['difficulty']].items()))
+  if quiz['correctCount'] == answers_list_length:
+    print paragraph, '\n\nCongratulations, you filled in all blanks correctly!'
+    quiz['running'] = False
+  else:
+    return False
+
+
+def onCorrectUserAnswer(question, correct_answer):
+  # When user gets question correct this function resets error count, adds 1 to correct count,
+  #   replaces question prompt with correct answer and prints new paragraph
+  # Inputs: question, and correct_answer
+  correct_plus_one = 1
+  err_count_restart = 0
+  quiz['errorCount'] = err_count_restart  
+  paragraph = quiz['paragraph']
+  quiz['correctCount'] += correct_plus_one
+  # print "quiz['correctCount']:\n", quiz['correctCount']
+  quiz['paragraph'] = paragraph.replace(question, correct_answer)
+  print "Your paragraph now reads:\n",quiz["paragraph"] 
+
+def onIncorrectUserAnswer():
+  # When user gets the question incorrect this function adds 1 to the error count
+  # prints incorrect prompt with remaining attempts count and reprints paragraph
+   max_tries = 5
+   errorc_plus_one = 1 #error count + 1 when incorrect
+   quiz['errorCount'] = quiz['errorCount'] + errorc_plus_one
+   attempts_remaining = str(abs(quiz['errorCount'] - max_tries))
+   error_count_num = '\nIncorrect, you have ' + attempts_remaining + ' attempts left!'
+   print error_count_num
+   print quiz["paragraph"]
+   
+
+def too_many_attempts():
+  # When the user uses up all attempts this function sets game['running'] to False and
+  #   runs question_loop to end game 
+  max_tries = 5
+  answers_list = sorted(list(ANSWERS[quiz['difficulty']].items()))
+  if quiz['errorCount'] == max_tries: # If you exceed max tries then quiz ends
+      quiz['running'] = False
+      question_loop(answers_list)
+      
   
 
 start_quiz()
